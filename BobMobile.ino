c25ea8f1e8fb3b2caabdd40d2da4fa080fdc3dbf
John Board <johnrobboard.com>
2015-08-17 07:17:52 +1000

    Comp code

--- BobMobile.ino
+++ BobMobile.ino
@@ -6,8 +6,11 @@
 #include <Servo.h> 
 
 /* Constants */
-const LEFT_MOTOR_PIN = 2;
-const RIGHT_MOTOR_PIN = 3;
+const int LEFT_MOTOR_PIN = 2;
+const int RIGHT_MOTOR_PIN = 3;
+const int BUTTON_PIN = 12;
+
+unsigned long t;
 
 /* Objects */
 Servo leftMotor;
@@ -16,10 +19,63 @@ Servo rightMotor;
 /* Program Code */
 
 void setup() {
+  Serial.begin(9600);
   startMotors(LEFT_MOTOR_PIN, RIGHT_MOTOR_PIN);
+
+  pinMode(BUTTON_PIN, INPUT);
+  digitalWrite(BUTTON_PIN, HIGH); // pull-up
+  /*startTimer();
+  while(true){
+    while (!handleButton()){}
+    Serial.println("Button up.");
+    Serial.println(getTime());
+    while(handleButton()){}
+    Serial.println("Button down.");
+    Serial.println(getTime());
+  }*/
+  while(!handleButton()){}
+  startTimer();
 }
 
-void loop() {}
+void beep(){
+  for(int x=0; x<400; x++){
+    analogWrite(5, 255);
+    delayMicroseconds(200);
+    analogWrite(5, 0);
+    delayMicroseconds(200);
+  }
+}
+
+boolean handleButton()
+{
+  int button_pressed = !digitalRead(BUTTON_PIN); // pin low -> pressed
+  return button_pressed;
+}
+
+void loop() {
+  beep();
+  delay(18351);
+  forward(40);
+  delay(2000);
+  stopMotors();
+  
+  
+  delay(25786);
+  backward(40);
+  delay(2000);  
+  stopMotors();
+
+  while(true){}
+  
+}
+
+void startTimer(){
+  t = millis();
+}
+
+int getTime(){
+  return millis()-t;
+}
 
 /* Driver Code */
 
@@ -52,10 +108,12 @@ void forward(int speed){
   //As speed is from 0-100 and range is from 0-1000 (1500-2500), input gets multipled by 10
   speed = speed * 10;
 
-  //1500 = neutral. 500 = full forward. 50 = half forward. 50*10 = 500. 1500-500 = 1000 - half forward.
-  speed = 1500-speed;
+  int speedL = (speed/100)*90;
+  int speedR = speed;
 
-  //Offset code here
+  //1500 = neutral. 500 = full forward. 50 = half forward. 50*10 = 500. 1500-500 = 1000 - half forward.
+  speedL = 1500+speedL;
+  speedR = 1500+speedR;  
 
   //Write speeds to motors
   setSpeeds(speedL, speedR);
@@ -72,11 +130,16 @@ void backward(int speed){
   //As speed is from 0-100 and range is from 0-1000 (1500-2500), input gets multipled by 10
   speed = speed * 10;
 
-  //1500 = neutral. 500 = full forward. 50 = half forward. 50*10 = 500. 1500-500 = 1000 - half forward.
-  speed = 1500+speed;
+  int speedL = speed;
+  int speedR = (speed/100)*95;
 
-  //Offset code here
+  
+  speedL = 1500-speedL;
+  speedR = 1500-speedR;
 
   //Write speeds to motors
   setSpeeds(speedL, speedR);
+
+
+  //1500 = neutral. 500
 }
